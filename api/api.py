8078522a15c727de48ce48ad14d7a4fd77c8cd41
kei130bbca <tibitibikei1018@gmail.com>
2021-09-12 21:18:01 +0900

    Editing sample data

--- api/api.py
+++ api/api.py
@@ -1,14 +1,14 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 
 db_list = [
-    {'title1': 'body1'},
-    {'title2': 'body2'},
-    {'title3': 'body3'},
-    {'title4': 'body4'},
-    {'title5': 'body5'},
+    {'title': 'title 1', 'body': 'body 1'},
+    {'title': 'title 2', 'body': 'body 2'},
+    {'title': 'title 3', 'body': 'body 3'},
+    {'title': 'title 4', 'body': 'body 4'},
+    {'title': 'title 5', 'body': 'body 5'},
 ]
 
 
@@ -18,7 +18,7 @@ def get_articles():
 
 @app.route('/article/<int:id>', methods=['GET'])
 def get_article(id):
-    return jsonify(db_list[int(id)])
+    return jsonify(db_list[id])
 
 
 if __name__ == '__main__':
