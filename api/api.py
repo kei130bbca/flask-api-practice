a404a443f1280e599b9a83de1b9938a72a54c47e
kei130bbca <tibitibikei1018@gmail.com>
2021-09-12 21:07:57 +0900

    Adding GET function

--- api/api.py
+++ api/api.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Hellow, World!'
+
+db_list = [
+    {'title1': 'body1'},
+    {'title2': 'body2'},
+    {'title3': 'body3'},
+    {'title4': 'body4'},
+    {'title5': 'body5'},
+]
+
+
+@app.route('/articles', methods=['GET'])
+def get_articles():
+    return jsonify(db_list)
+
+@app.route('/article/<int:id>', methods=['GET'])
+def get_article(id):
+    return jsonify(db_list[int(id)])
+
 
 if __name__ == '__main__':
     app.run(debug=True)
